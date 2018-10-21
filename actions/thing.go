7b8333dd56f421804ced999ac92e855fac683ef6
Parham Alvani <parham.alvani@gmail.com>
2018-10-21 08:46:55 +0330

    Adds thing list and thing creation apis.

--- actions/thing.go
+++ actions/thing.go
@@ -14,25 +14,93 @@
 package actions
 
 import (
+	"fmt"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 
+	"github.com/I1820/backend/models"
+	"github.com/I1820/types"
+	"github.com/go-resty/resty"
 	"github.com/gobuffalo/buffalo"
-	"github.com/gobuffalo/envy"
 )
 
-// ThingsHandler proxies each thing request to pm component.
-func ThingsHandler(c buffalo.Context) error {
-	path := c.Param("path")
+// ThingsResource controls the users access to things and proxies their request to pm
+type ThingsResource struct {
+	buffalo.Resource
+	pmclient *resty.Client
+}
 
-	url, err := url.Parse(envy.Get("PM_URL", "http://127.0.0.1:1375"))
-	if err != nil {
-		return c.Error(http.StatusInternalServerError, err)
+// List lists things of given project. This function is mapped to the path
+// GET /projects/{project_id}/things
+func (v ThingsResource) List(c buffalo.Context) error {
+	projectID := c.Param("project_id")
+
+	// get user from request context
+	u, ok := c.Value("user").(models.User)
+	if !ok {
+		return c.Error(http.StatusInternalServerError, fmt.Errorf("There is no valid user in request context"))
 	}
 
-	c.Request().URL.Path = path
-	return buffalo.WrapHandler(
-		httputil.NewSingleHostReverseProxy(url),
-	)(c)
+	for _, p := range u.Projects {
+		if p == projectID {
+			var ts []types.Thing
+
+			// gets project things from pm
+			// I1820/pm/ThingsResource.List
+			resp, err := v.pmclient.R().SetResult(&ts).SetPathParams(map[string]string{
+				"projectID": projectID,
+			}).Get("api/projects/{projectID}/things")
+			if err != nil {
+				return c.Error(http.StatusInternalServerError, err)
+			}
+
+			if resp.IsError() {
+				return c.Render(resp.StatusCode(), r.JSON(resp.Error()))
+			}
+
+			return c.Render(http.StatusOK, r.JSON(ts))
+		}
+	}
+
+	return c.Error(http.StatusNotFound, fmt.Errorf("Project %s not found", projectID))
+}
+
+// Create creates a new thing in pm. This function is mapped to the
+// path POST /projects/{project_id}/things
+func (v ThingsResource) Create(c buffalo.Context) error {
+	projectID := c.Param("project_id")
+
+	// generic request so we can change it when pm `thingReq` is changed.
+	var rq interface{}
+	if err := c.Bind(&rq); err != nil {
+		return c.Error(http.StatusBadRequest, err)
+	}
+
+	// get user from request context
+	u, ok := c.Value("user").(models.User)
+	if !ok {
+		return c.Error(http.StatusInternalServerError, fmt.Errorf("There is no valid user in request context"))
+	}
+
+	for _, p := range u.Projects {
+		if p == projectID {
+			var t types.Thing
+
+			// creates a thing in pm
+			// I1820/pm/ThingsResource.Create
+			resp, err := v.pmclient.R().SetBody(rq).SetResult(&t).SetPathParams(map[string]string{
+				"projectID": projectID,
+			}).Post("api/projects/{projectID}/things")
+			if err != nil {
+				return c.Error(http.StatusInternalServerError, err)
+			}
+
+			if resp.IsError() {
+				return c.Render(resp.StatusCode(), r.JSON(resp.Error()))
+			}
+
+			return c.Render(http.StatusOK, r.JSON(t))
+		}
+	}
+
+	return c.Error(http.StatusNotFound, fmt.Errorf("Project %s not found", projectID))
 }
