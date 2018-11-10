be67c24b7210c4dc286a7cd9428c182ed51298af
Parham Alvani <parham.alvani@gmail.com>
2018-11-10 10:32:34 +0330

    Adds fields for access and refresh tokens.

--- models/user.go
+++ models/user.go
@@ -17,11 +17,14 @@ package models
 // Each user can have many projects. projects can exist for multiple users at the same time.
 // I1820 do not store tokens in database, tokens just check by hand in auth middleware.
 type User struct {
-	Username  string   `json:"username" bson:"username"`   // like 1995parham without any restriction and must be unique in the system
-	Firstname string   `json:"firstname" bson:"firstname"` // UTF-8 support
-	Lastname  string   `json:"lastname" bson:"lastname"`   // UTF-8 support
-	Email     string   `json:"email" bson:"email"`         // valid email address
-	Password  string   `json:"password,omitempty" bson:"password"`
-	Token     string   `json:"token,omitempty" bson:"-"`
-	Projects  []string `json:"projects" bson:"projects"`
+	Username  string `json:"username" bson:"username"`   // like 1995parham without any restriction and must be unique in the system
+	Firstname string `json:"firstname" bson:"firstname"` // UTF-8 support
+	Lastname  string `json:"lastname" bson:"lastname"`   // UTF-8 support
+	Email     string `json:"email" bson:"email"`         // valid email address
+	Password  string `json:"password,omitempty" bson:"password"`
+
+	AccessToken  string `json:"access_token,omitempty" bson:"-"`  // token is uesed to access I1820 resources
+	RefreshToken string `json:"refresh_token,omitempty" bson:"-"` // token is used to generate a new token when the old one is expired.
+
+	Projects []string `json:"projects" bson:"projects"` // list of user owned projects
 }
