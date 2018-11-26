acfd2cfe3b0f044e63fff954d60a9082fd2fff72
Parham Alvani <parham.alvani@gmail.com>
2018-11-26 12:53:50 +0330

    Adds users resource.

--- actions/user.go
+++ actions/user.go
@@ -1,1 +1,18 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 26-11-2018
+ * |
+ * | File Name:     user.go
+ * +===============================================
+ */
+
 package actions
+
+// UsersResource handles operations on users. These operations only available to admins.
+type UsersResource struct {
+}
